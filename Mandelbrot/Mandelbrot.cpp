d5f8c17b9169661d834559a507aa98ea4e9e4b03
Gordon Stangler <gordon.stangler@gmail.com>
2019-11-05 17:17:54 -0500

    Update Mandelbrot.cpp
    .Update to Mandelbrot.cpp

--- Mandelbrot/Mandelbrot.cpp
+++ Mandelbrot/Mandelbrot.cpp
@@ -1,13 +1,11 @@
 #include <complex>
 #include <cstdio>
 
-typedef std::complex<double> complex;
-
-int MandelbrotCalculate(complex c, int maxiter)
+int MandelbrotCalculate(std::complex<double> c, int maxiter)
 {
 	// iterates z = z + c until |z| >= 2 or maxiter is reached,
 	// returns the number of iterations.
-	complex z = c;
+	std::complex<double> z = c;
 	int n=0;
 	for(; n<maxiter; ++n)
 	{
@@ -20,15 +18,15 @@ int MandelbrotCalculate(complex c, int maxiter)
 int main()
 {
 	const int width = 78, height = 44, num_pixels = width*height;
-	const complex center(-.7, 0), span(2.7, -(4/3.0)*2.7*height/width);
-	const complex begin = center-span/2.0;//, end = center+span/2.0;
+	const std::complex<double> center(-.7, 0), span(2.7, -(4/3.0)*2.7*height/width);
+	const  begin = center-span/2.0;
 	const int maxiter = 100000;
 
 	#pragma omp parallel for ordered schedule(dynamic)
 	for(int pix=0; pix<num_pixels; ++pix)
 	{
 		const int x = pix%width, y = pix/width;
-		complex c = begin + complex(x * span.real() / (width +1.0), y * span.imag() / (height+1.0));
+		std::complex<double> c = begin + std::complex<double> (x * span.real() / (width +1.0), y * span.imag() / (height+1.0));
          
 		int n = MandelbrotCalculate(c, maxiter);
 		if(n == maxiter) n = 0;
