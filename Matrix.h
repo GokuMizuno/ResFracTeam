691c2acce7d3989aeade3a38d74aa04e83c6ac6b
Gordon Stangler <gordon.stangler@gmail.com>
2019-11-07 09:11:04 -0500

    Create Matrix.h
    A simple and sanitized version of  a  FTL matrix file.

---
+++ Matrix.h
@@ -0,0 +1,46 @@
+//simple matrix class
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <vector>
+
+template<typename Object>
+class Matrix
+{
+	public:
+		matrix(int rows, int columns) : array(rows)
+		{
+			for(auto &thisRow : array)
+				thisRow.resize(columns);
+		}
+
+		matrix(vector<vector<Object>> v : array{v}
+		{}
+
+		matrix(vector<vector<Object>> &&v : array{std::move(v)}
+		{}
+
+		const vector<Object> & operator [](int row) const
+		{
+			return array[row];
+		}
+
+		const vector<Object> & operator [](int row)
+		{
+			return array[row];
+		}
+
+		int numRows() const
+		{
+			return array.size();
+		}
+
+		int numCols() const
+		{
+			return numRows() ? array[0].size : 0;
+		}
+
+	private:
+		vector<vector<Object>> array;
+};
+#endif
