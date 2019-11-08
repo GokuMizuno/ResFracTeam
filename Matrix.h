63178a9104fd868d307816b9e84b7628c3654242
Gordon Stangler <gordon.stangler@gmail.com>
2019-11-07 20:11:45 -0500

    Update Matrix.h
    Remove extra white space.

--- Matrix.h
+++ Matrix.h
@@ -1,6 +1,7 @@
 //simple matrix class
 #ifndef MATRIX_H
 #define MATRIX_H
+#pragma once
 
 #include <vector>
 #include <omp>
@@ -16,23 +17,12 @@ class Matrix
 				thisRow.resize(columns);
 		}
 
-		matrix(vector<vector<Object>> v : array{v}
-		{}
-
-		matrix(vector<vector<Object>> &&v : array{std::move(v)}
-		{}
-
-		const vector<Object> & operator [](int row) const
-		{  return array[row];  }
-
-		const vector<Object> & operator [](int row)
-		{  return array[row];  }
-
-		int numRows() const
-		{  return array.size();  }
-
-		int numCols() const
-		{  return numRows() ? array[0].size : 0;  }
+		matrix(vector<vector<Object>> v : array{v} {}
+		matrix(vector<vector<Object>> &&v : array{std::move(v)} {}
+		const vector<Object> & operator [](int row) const {  return array[row];  }
+		const vector<Object> & operator [](int row) {  return array[row];  }
+		int numRows() const {  return array.size();  }
+		int numCols() const {  return numRows() ? array[0].size : 0;  }
 
 	private:
 		vector<vector<Object>> array;
